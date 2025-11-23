dbaa7ebc18c8d5f42135d290e949af30d6b03bbd
linanagm <lina.nagm@gmail.com>
2025-11-23 15:29:00 +0300

    feat(cli): add react-generate-cli setup and component template

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Home from './components/Home/Home'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    
       <div>
+        <Home />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react'
+import styles from './Home.module.css'
+
+export default function Home() {
+
+    let [ count , setCount ] = useState()
+    
+      useEffect (() => {}, [])
+    
+      return (
+    <div>
+      home
+    </div>
+  )
+}
