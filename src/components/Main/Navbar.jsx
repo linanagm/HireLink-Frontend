b704ef076e3f183c4bd72b6a3cc13af47ecb2527
linanagm <lina.nagm@gmail.com>
2025-11-28 11:46:34 +0300

    feat: switch from ESLNT to Biome - adds(configue , scripts)

--- src/components/Main/Navbar.jsx
+++ src/components/Main/Navbar.jsx
@@ -19,10 +19,10 @@ export default function NavbarComponent() {
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-4 ">
   
         {/* logo */}
-      <a href="home" className=" flex items-center space-x-3 rtl:space-x-reverse">
+      <NavLink to={"home"} className=" flex items-center space-x-3 rtl:space-x-reverse">
             <img src={logo} className="h-7" alt="HireLink Logo" />
             {/* <span className="self-center text-xl text-heading font-semibold whitespace-nowrap">HireLink</span> */}
-      </a>
+      </NavLink>
       
         {/* mobile collapse button */}
       <div className="inline-flex md:order-2 md:hidden lg:hidden space-x-3 md:space-x-0 rtl:space-x-reverse">
@@ -44,15 +44,15 @@ export default function NavbarComponent() {
         <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-default rounded-base bg-neutral-secondary-soft md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-neutral-primary">
       
           <li>
-            <a href="home" className="block py-2 px-3  bg-brand rounded md:bg-transparent md:text-fg-brand md:p-0" aria-current="page">Home</a>
+            <a href="home" className="block py-2 px-3  bg-brand rounded md:bg-transparent md:text-fg-brand md:p-0  text-fuchsia-800 hover:text-fuchsia-600" aria-current="page">Home</a>
           </li>
       
           <li>
-            <a href="about" className="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent">About</a>
+            <a href="about" className="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent  text-fuchsia-800 hover:text-fuchsia-600">About</a>
           </li>
       
           <li>
-            <a href="contact" className="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent">Contact</a>
+            <a href="contact" className="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent  text-fuchsia-800 hover:text-fuchsia-600">Contact</a>
           </li>
 
           
