391a9d4c113a196ec21d5e3569fbd0f5627fe17d
linanagm <lina.nagm@gmail.com>
2026-01-17 19:56:41 +0300

    feat: install tanStack react query for server state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeInit } from "../.flowbite-react/init";
 import { AuthProvider } from "./context/AuthContext";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
+	const query = new QueryClient();
+
 	return (
-		<>
+		<QueryClientProvider client={query}>
 			<ThemeInit />
 			<AuthProvider>
 				<AppRoutes />
 			</AuthProvider>
-		</>
+		</QueryClientProvider>
 	);
 }
 
